6a0c5ec50f08d94c5d661371f2b00129c61b0401
vighnesh2709 <bvighnesh27@gmail.com>
2025-05-28 22:22:32 +0530

    some mods made

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-const userName = "Rob-"+Math.floor(Math.random() * 100000)
+const userName = "Vighnesh"
 const password = "x";
 document.querySelector('#user-name').innerHTML = userName;
 
-//if trying it on a phone, use this instead...
-const socket = io.connect('https://192.168.1.5:8181/',{
+
+const socket = io.connect('https://192.168.1.39:8181/',{
 // const socket = io.connect('https://localhost:8181/',{
     auth: {
         userName,password
@@ -13,9 +13,9 @@ const socket = io.connect('https://192.168.1.5:8181/',{
 const localVideoEl = document.querySelector('#local-video');
 const remoteVideoEl = document.querySelector('#remote-video');
 
-let localStream; //a var to hold the local video stream
-let remoteStream; //a var to hold the remote video stream
-let peerConnection; //the peerConnection that the two clients use to talk
+let localStream; 
+let remoteStream; 
+let peerConnection; 
 let didIOffer = false;
 
 let peerConfiguration = {
@@ -29,21 +29,21 @@ let peerConfiguration = {
     ]
 }
 
-//when a client initiates a call
-const call = async e=>{
-    await fetchUserMedia();
 
-    //peerConnection is all set with our STUN servers sent over
+
+async function call(){
+    console.log("Button Clicked to Call")
+    await fetchUserMedia();
+    console.log("User Media Fetched")
     await createPeerConnection();
 
-    //create offer time!
     try{
         console.log("Creating offer...")
         const offer = await peerConnection.createOffer();
-        console.log(offer);
+        console.log("Offer" + offer);
         peerConnection.setLocalDescription(offer);
         didIOffer = true;
-        socket.emit('newOffer',offer); //send offer to signalingServer
+        socket.emit('newOffer',offer);
     }catch(err){
         console.log(err)
     }
@@ -51,43 +51,40 @@ const call = async e=>{
 }
 
 
-const answerOffer = async(offerObj)=>{
+async function answerOffer(offerObj){
     await fetchUserMedia()
     await createPeerConnection(offerObj);
-    const answer = await peerConnection.createAnswer({}); //just to make the docs happy
-    await peerConnection.setLocalDescription(answer); //this is CLIENT2, and CLIENT2 uses the answer as the localDesc
-    console.log(offerObj)
-    console.log(answer)
-    // console.log(peerConnection.signalingState) //should be have-local-pranswer because CLIENT2 has set its local desc to it's answer (but it won't be)
-    //add the answer to the offerObj so the server knows which offer this is related to
+    const answer = await peerConnection.createAnswer({}); 
+    await peerConnection.setLocalDescription(answer); 
+    console.log("offer Object" + offerObj)
+    console.log(" Answer" + answer)
+   
     offerObj.answer = answer 
-    //emit the answer to the signaling server, so it can emit to CLIENT1
-    //expect a response from the server with the already existing ICE candidates
+ 
     const offerIceCandidates = await socket.emitWithAck('newAnswer',offerObj)
     offerIceCandidates.forEach(c=>{
         peerConnection.addIceCandidate(c);
         console.log("======Added Ice Candidate======")
     })
-    console.log(offerIceCandidates)
+    console.log("Ice Candidates"+ offerIceCandidates)
 }
 
-const addAnswer = async(offerObj)=>{
-    //addAnswer is called in socketListeners when an answerResponse is emitted.
-    //at this point, the offer and answer have been exchanged!
-    //now CLIENT1 needs to set the remote
+
+async function addAnswer(offerObj){
     await peerConnection.setRemoteDescription(offerObj.answer)
-    // console.log(peerConnection.signalingState)
 }
 
-const fetchUserMedia = ()=>{
+
+function fetchUserMedia(){
     return new Promise(async(resolve, reject)=>{
         try{
             const stream = await navigator.mediaDevices.getUserMedia({
                 video: true,
-                // audio: true,
+                audio: true,
             });
             localVideoEl.srcObject = stream;
-            localStream = stream;    
+            localStream = stream;
+            console.log("Stream Stream Stream" + localStream);    
             resolve();    
         }catch(err){
             console.log(err);
@@ -96,18 +93,16 @@ const fetchUserMedia = ()=>{
     })
 }
 
-const createPeerConnection = (offerObj)=>{
+
+async function createPeerConnection(offerObj){
     return new Promise(async(resolve, reject)=>{
-        //RTCPeerConnection is the thing that creates the connection
-        //we can pass a config object, and that config object can contain stun servers
-        //which will fetch us ICE candidates
+
         peerConnection = await new RTCPeerConnection(peerConfiguration)
         remoteStream = new MediaStream()
         remoteVideoEl.srcObject = remoteStream;
 
 
         localStream.getTracks().forEach(track=>{
-            //add localtracks so that they can be sent once the connection is established
             peerConnection.addTrack(track,localStream);
         })
 
@@ -138,20 +133,22 @@ const createPeerConnection = (offerObj)=>{
         })
 
         if(offerObj){
-            //this won't be set when called from call();
-            //will be set when we call from answerOffer()
-            // console.log(peerConnection.signalingState) //should be stable because no setDesc has been run yet
             await peerConnection.setRemoteDescription(offerObj.offer)
-            // console.log(peerConnection.signalingState) //should be have-remote-offer, because client2 has setRemoteDesc on the offer
         }
         resolve();
     })
 }
 
-const addNewIceCandidate = iceCandidate=>{
+
+function addNewIceCandidate(){
     peerConnection.addIceCandidate(iceCandidate)
     console.log("======Added Ice Candidate======")
 }
 
 
 document.querySelector('#call').addEventListener('click',call)
+
+// browser embedded in TV (always listen)
+// Server for coms
+// web Browser application on phone
+// one server to listen to the voice commands
--- socketListeners.js
+++ socketListeners.js
@@ -1,11 +1,10 @@
 
-//on connection get all available offers and call createOfferEls
+
 socket.on('availableOffers',offers=>{
     console.log(offers)
     createOfferEls(offers)
 })
 
-//someone just made a new offer and we're already here - call createOfferEls
 socket.on('newOfferAwaiting',offers=>{
     createOfferEls(offers)
 })
@@ -21,7 +20,6 @@ socket.on('receivedIceCandidateFromServer',iceCandidate=>{
 })
 
 function createOfferEls(offers){
-    //make green answer button for this new offer
     const answerEl = document.querySelector('#answer');
     offers.forEach(o=>{
         console.log(o);
